e728d30534b4a831ca38b84a3d3ba3210e5ec9ba
dudosa-1 <kirillmdv.2007@gmail.com>
2026-02-18 00:21:16 +0700

    задание 3.1

--- labs/lab08_pointers_arrays/Lab08/src/main.cpp
+++ labs/lab08_pointers_arrays/Lab08/src/main.cpp
@@ -8,12 +8,85 @@
 // - числа через пробел
 // - если несколько строк — в фиксированном порядке
 
+int Mass_1[10], Mass_2[10];
+int* Mass_3, * Mass_4;
+
+
 void zadanie_2(int a, int b)
 {
     int* x = &a;
     std::cout << *x * 2 << std::endl;
     int& ref = b;
     std::cout << ref + 10 << std::endl;
+}
+
+void mass_tip_1()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Mass_1[i] = i * i;
+    }
+}
+
+void mass_tip_2()
+{
+    int* p;
+    p = Mass_2;
+    for (int i = 0; i < 10; i++)
+    {
+        *p = i * i;
+        p++;
+    }
+}
+
+void mass_tip_3()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Mass_3[i] = i * i;
+    }
+}
+
+void mass_tip_4()
+{
+    int* p;
+    p = Mass_4;
+    for (int i = 0; i < 10; i++)
+    {
+        *p = i * i;
+        p++;
+    }
+}
+
+void zadanie_3()
+{
+    mass_tip_1();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << Mass_1[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    mass_tip_2();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << Mass_2[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    mass_tip_3();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << Mass_3[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    mass_tip_4();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << Mass_4[i] << ' ';
+    }
+    std::cout << std::endl;
 }
 
 int main()
@@ -27,7 +100,11 @@ int main()
     zadanie_2(2, 4);
     // Упражнение 3: заполнить массивы (10 элементов) разными способами.
     // TODO: реализуйте 4 варианта заполнения и выведите 10 значений для каждого.
-
+    Mass_3 = new int[10];
+    Mass_4 = new int[10];
+    zadanie_3();
+    delete[] Mass_3;
+    delete[] Mass_4;
     // Упражнение 4: линейный самоадресуемый список из 10 элементов.
     // TODO: сформируйте список и выведите элементы по порядку.
     return 0;
