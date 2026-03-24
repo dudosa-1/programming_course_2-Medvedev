69f67a2fe727a71aa000e2e05140b6442d222179
dudosa-1 <kirillmdv.2007@gmail.com>
2026-03-25 02:44:43 +0700

    ферзи начало

--- labs/lab12_combinatorics/Lab12/src/main.cpp
+++ labs/lab12_combinatorics/Lab12/src/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
-
+#include <cmath>
 // Lab 12
-// TODO: реализуйте решение по заданию в labs/lab12_combinatorics/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
-void Q(int arr[], int k) {
-    for (int i = 0; i < k; i++) arr[i] = 0;
+int c1 = 0;
+int c2 = 0;
 
+void sp1(int** D1, int str, int stl, int n) {
+	if (str == n) {
+		c1++;
+		print();
+		return;
+	}
+	for (stl = 0; stl < n; stl++) {
+		if (lob()) {
+			D1[str][stl] = 1;
+		}
+
+	}
 }
 
+int main(){
+	int n;
+	std::cin >> n;
+	if (n <= 0) return 0;
+	int** D1 = new int* [n];
+	for (int i = 0; i < n; i++) {
+		D1[i] = new int[n];
+		for (int j = 0; j < n; j++) {
+			D1[i][j] = 0;
+		}
+	}
+	int* D2 = new int[n];
 
-int main() {
-    int n;
-    int D[n];
-    for 
-    // TODO
-    return 0;
+
 }
