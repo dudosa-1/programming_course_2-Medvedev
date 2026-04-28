467c8825e3711466bff3a46f79fa09f9df62aa50
dudosa-1 <kirillmdv.2007@gmail.com>
2026-04-29 02:18:03 +0700

    свершилось лаба: 14 задание: 1.1

--- labs/lab14_graphs/Lab14/src/main.cpp
+++ labs/lab14_graphs/Lab14/src/main.cpp
@@ -1,13 +1,61 @@
 #include <iostream>
+#include <fstream>
 
-// Lab 14
-// TODO: реализуйте решение по заданию в labs/lab14_graphs/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
+void zad_1(const char* infile, const char* outfile) {
+    std::ifstream fin(infile);
+    std::ofstream fout(outfile);
+
+    int n, m;
+    fin >> n >> m;
+
+    int** D = new int* [n];
+    for (int i = 0; i < n; i++) {
+        D[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            D[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        fin >> a >> b;
+        a--; b--;
+        D[a][b] = 1;
+        D[b][a] = 1;
+    }
+
+    fin.close();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            fout << D[i][j];
+            if (j != n - 1) fout << " ";
+        }
+        fout << std::endl;
+    }
+
+    fout.close();
+
+    for (int i = 0; i < n; i++) delete[] D[i];
+    delete[] D;
+}
+
 int main() {
-    // TODO
+    const char* inputfile = "input.txt";
+    const char* output_1 = "output_1.txt";
+    const char* output_2 = "output_2.txt";
+    const char* output_3 = "output_3.txt";
+
+    // Проверяем существование входного файла
+    std::ifstream test(inputfile);
+    if (!test) {
+        std::cout << "FILE NOT" << std::endl;
+        return 0;
+    }
+    test.close();
+
+    zad_1(inputfile, output_1);
+
+    std::cout << "comp" << std::endl;
     return 0;
 }
